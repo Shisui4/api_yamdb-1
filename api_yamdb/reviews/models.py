ad90cc9dfdf83ac770da1fb969fb04a913a65fb7
Ksenia <senya.sidorovich@gmail.com>
2021-11-30 10:13:43 +0300

    User model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
-    pass
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+        null=True,
+    )
+    role = models.CharField(
+        'Статус пользователя',
+        max_length=20,
+        choices=(('m','moderator'), ('a','admin'), ('u','user')),
+        default='user'
+    )
+
+    def __str__(self):
+        return self.username
