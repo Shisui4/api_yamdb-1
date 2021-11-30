57f71afa1940c0196628cddced94395e8b9e4f1a
Ksenia <senya.sidorovich@gmail.com>
2021-11-30 13:02:39 +0300

    Corrected User model, added UserSerializer

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,6 +3,12 @@ from django.db import models
 
 
 class User(AbstractUser):
+    first_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(
+        'email',
+        unique=True,
+        max_length=254
+    )
     bio = models.TextField(
         'Биография',
         blank=True,
@@ -11,7 +17,7 @@ class User(AbstractUser):
     role = models.CharField(
         'Статус пользователя',
         max_length=20,
-        choices=(('m','moderator'), ('a','admin'), ('u','user')),
+        choices=(('moderator','m'), ('admin','a'), ('user','u')),
         default='user'
     )
 
