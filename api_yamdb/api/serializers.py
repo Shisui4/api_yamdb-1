f3cb58a161fe3fb84c2610767024c0759558aebf
Серёжа <shisui4@mail.ru>
2021-12-02 04:15:14 +1000

    Commit after create models and serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from reviews.models import User
+from reviews.models import User, Categories, Genre, Title
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,4 +13,45 @@ class UserSerializer(serializers.ModelSerializer):
         if name == 'me':
             raise serializers.ValidationError(
                 'Это имя не может быть использовано!')
-        return name 
+        return name
+
+
+class CategoriesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('name', 'slug')
+        model = Categories
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('name', 'slug')
+        model = Genre
+
+
+class TitleViewSerializer(serializers.ModelSerializer):
+
+    genre = GenreSerializer(many=True, read_only=True)
+    category = CategoriesSerializer(read_only=True)
+
+    class Meta:
+        fields = ('id', 'name', 'release', 'description',
+                  'genre', 'category')
+        meta = Title
+
+
+class TitleCreateSerializer(serializers.ModelSerializer):
+
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Categories.objects.all()
+    )
+    genre = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Genre.objects.all()
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Title
