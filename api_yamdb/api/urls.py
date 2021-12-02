369a0840e348244bef617006c736fe087217bb2b
Ksenia <senya.sidorovich@gmail.com>
2021-12-02 19:53:25 +0300

    correction

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -15,7 +15,6 @@ router.register(
      r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
      CommentViewSet, basename='comment'
 
-
 urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/auth/signup/', sign_up, name='signup'),
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -6,7 +6,6 @@ from django.core.mail import send_mail
 from rest_framework import filters
 from rest_framework import status
 from rest_framework import viewsets
-<<<<<<< HEAD
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -15,10 +14,6 @@ from .serializers import SignUpSerializer, UserSerializer
 
 from_email = 'from@yamdb.com'
 subject = 'confirmation code'
-=======
-from reviews.models import User, Title, Review
-from .serializers import CommentSerializer, UserSerializer, ReviewSerializer
->>>>>>> master
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -29,7 +24,6 @@ class UserViewSet(viewsets.ModelViewSet):
     search_fields = ('username',)
 
 
-<<<<<<< HEAD
 """@api_view(['POST'])
 def sign_up(request):
     serializer =SignUpSerializer(data=request.data)
@@ -42,8 +36,11 @@ def sign_up(request):
     serializer =SignUpSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     email = serializer.validated_data['email']
+    user, status = User.objects.get_or_create(**validated_data)
+    if status and user.confirmation_code == True:
+        raise  
     if not User.objects.filter(email=email).exists():
-        user = User.objects.create(**validated_data)   
+        user = User.objects.create(**validated_data, confirmation_code=True)   
     confirmation_code = str(uuid.uuid3(uuid.NAMESPACE_X500, email))
     send_mail(
         subject=subject,
@@ -53,34 +50,3 @@ def sign_up(request):
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-     
-=======
-class ReviewViewSet(viewsets.ModelViewSet):
-    serializer_class = ReviewSerializer
-
-    def get_queryset(self):
-        title_id = self.kwargs.get('title_id')
-        title = get_object_or_404(Title, id=title_id)
-        return title.reviews.all()
-
-    def perform_create(self, serializer):
-        title_id = self.kwargs.get('title_id')
-        title = get_object_or_404(Title, id=title_id)
-        user = self.request.user
-        serializer.save(author=user, title=title)
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    serializer_class = CommentSerializer
-
-    def get_queryset(self):
-        review_id = self.kwargs.get('review_id')
-        review = get_object_or_404(Review, id=review_id)
-        return review.comments.all()
-
-    def perform_create(self, serializer):
-        review_id = self.kwargs.get('review_id')
-        review = get_object_or_404(Review, id=review_id)
-        user = self.request.user
-        serializer.save(author=user, review=review)
->>>>>>> master
