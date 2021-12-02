3d9ac983f1cda9da9ab7ff296eb7ab20cb1a1da8
Дмитрий Муратов <myratov91@yandex.ru>
2021-12-02 10:25:15 +0300

    add ReviewViewSet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,1 +1,21 @@
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
+
+from .serializers import ReviewSerializer
+from reviews.models import Title
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        user = self.request.user
+        serializer.save(author=user, title=title)
 
