a1ad3a7942722d25af5965b7f94039d366b784b6
Дмитрий Муратов <myratov91@yandex.ru>
2021-12-02 11:25:10 +0300

    add CommentViewSet

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,19 +1,19 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet, ReviewViewSet
-
+from .views import CommentViewSet, UserViewSet, ReviewViewSet
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
 router.register(
-     r'titles/(?P<title_id>\d+)/reviews',
-     ReviewViewSet, basename='review'
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet, basename='review'
+)
+router.register(
+     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+     CommentViewSet, basename='comment'
 )
 
 urlpatterns = [
     path('v1/', include(router.urls)),
 ]
-
-
-
